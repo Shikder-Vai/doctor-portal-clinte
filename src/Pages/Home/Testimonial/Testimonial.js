4ef843066419b002a275b2aafc186cbb23aa848d
unknown <Shikder-Vai>
2022-05-21 18:09:35 +0600

    testmonial bg added

--- src/Pages/Home/Testimonial/Testimonial.js
+++ src/Pages/Home/Testimonial/Testimonial.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Reviews from "./Reviews";
+import quote from "../../../assets/icons/quote.svg";
 
 const Testimonial = () => {
   return (
-    <div className="text-primary mx-10">
-      <div className="my-12">
+    <div className="text-primary mx-10 ">
+      <div
+        style={{ backgroundImage: `url(${quote})` }}
+        className="my-12 bg-right-top bg-no-repeat bg-contain"
+      >
         <h3>Testimonial</h3>
         <h1 className="text-5xl font-bold text-accent">
           What Our Patients Says
