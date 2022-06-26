ebf6998e9816292cc7f81d0e0fe89147452cc652
unknown <Shikder-Vai>
2022-06-26 13:56:48 +0600

    login form  done

--- src/Pages/Home/Testimonial/Reviews.js
+++ src/Pages/Home/Testimonial/Reviews.js
@@ -4,6 +4,7 @@ import Review from "./Review";
 const Reviews = () => {
   const reviews = [
     {
+      _id: 1,
       reviews:
         "Best of all: they're built to last, no matter how many times you jam a cable down an iron pipe or drag a reel out of a truck.",
       img: "https://i.ibb.co/C6554nF/people2.png",
@@ -11,6 +12,7 @@ const Reviews = () => {
       country: "California",
     },
     {
+      _id: 2,
       reviews:
         "Best of all: they're built to last, no matter how many times you jam a cable down an iron pipe or drag a reel out of a truck.",
       img: "https://i.ibb.co/SxBv34H/people1.png",
@@ -18,6 +20,7 @@ const Reviews = () => {
       country: "California",
     },
     {
+      _id: 3,
       reviews:
         "Best of all: they're built to last, no matter how many times you jam a cable down an iron pipe or drag a reel out of a truck.",
       img: "https://i.ibb.co/C6554nF/people2.png",
@@ -28,7 +31,7 @@ const Reviews = () => {
   return (
     <div className="grid p-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
       {reviews.map((review) => (
-        <Review key={review.index} review={review} />
+        <Review key={review._id} review={review} />
       ))}
     </div>
   );
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,105 @@
 import React from "react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+  if (user) {
+    console.log(user);
+  }
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
-    <div>
-      <h1>This is Login</h1>
+    <div className=" flex h-screen justify-center items-center">
+      <div class="card w-96 bg-base-100 shadow-xl">
+        <div class="card-body">
+          <h2 class=" text-center text-2xl font-bold">Login</h2>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div class="form-control w-full max-w-xs">
+              <label class="label">
+                <span class="label-text">Email</span>
+              </label>
+              <input
+                {...register("email", {
+                  required: {
+                    value: true,
+                    message: "Email address required",
+                  },
+                  pattern: {
+                    value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                    message: "Enter valid email",
+                  },
+                })}
+                type="text"
+                placeholder="Type here"
+                class="input input-bordered w-full max-w-xs"
+              />
+              <label class="label">
+                {errors.email?.type === "required" && (
+                  <span class="label-text-alt text-red-500">
+                    {errors.email.message}
+                  </span>
+                )}
+                {errors.email?.type === "pattern" && (
+                  <span class="label-text-alt text-red-500">
+                    {errors.email.message}
+                  </span>
+                )}
+              </label>
+            </div>
+            <div class="form-control w-full max-w-xs">
+              <label class="label">
+                <span class="label-text">Password</span>
+              </label>
+              <input
+                {...register("password", {
+                  required: {
+                    value: true,
+                    message: "Password address required",
+                  },
+                  minLength: {
+                    value: 6,
+                    message: "Must be less then 6 characters or longer",
+                  },
+                })}
+                type="text"
+                placeholder="Type here"
+                class="input input-bordered w-full max-w-xs"
+              />
+              <label class="label">
+                {errors.password?.type === "required" && (
+                  <span class="label-text-alt text-red-500">
+                    {errors.password.message}
+                  </span>
+                )}
+                {errors.password?.type === "minLength" && (
+                  <span class="label-text-alt text-red-500">
+                    {errors.password.message}
+                  </span>
+                )}
+              </label>
+            </div>
+
+            <input
+              className="btn text-white btn-secondary w-full max-w-xs "
+              type="submit"
+              value={"Login"}
+            />
+          </form>
+          <div class="divider">OR</div>
+
+          <button onClick={() => signInWithGoogle()} class="btn btn-outline">
+            Continue With Google
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
